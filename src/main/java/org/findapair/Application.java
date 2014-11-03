5657b865ff95545103d10d3e0d2b40bb2a56b4e2
Sandro Mancuso <sandromancuso@yahoo.com>
2014-11-03 11:50:20 +0200

    Added the available pairs page

--- src/main/java/org/findapair/Application.java
+++ src/main/java/org/findapair/Application.java
@@ -8,17 +8,19 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Application {
 
 	public static void main(String[] args) {
-		get("/", (req, res) -> indexPage());
+		get("/", (req, res) -> staticPage("index.html"));
+		post("/pairs", (req, res) -> staticPage("pairs.html"));
 	}
 
-	private static String indexPage() {
+	private static String staticPage(String pageName) {
 		try {
 
-			URI indexUri = Application.class.getClassLoader().getResource("pages/index.html").toURI();
+			URI indexUri = Application.class.getClassLoader().getResource("pages/"+ pageName).toURI();
 			Path indexPage = Paths.get(indexUri);
 			return new String(Files.readAllBytes(indexPage));
 
