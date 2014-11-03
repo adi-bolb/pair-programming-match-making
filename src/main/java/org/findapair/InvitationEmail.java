24357b4716becfe6af0d3cd74fd4c2e11652f54a
Sandro Mancuso <sandromancuso@yahoo.com>
2014-11-03 16:33:17 +0200

    Add the accept/reject links to the emails.

--- src/main/java/org/findapair/InvitationEmail.java
+++ src/main/java/org/findapair/InvitationEmail.java
@@ -5,7 +5,9 @@ public class InvitationEmail {
 		String from = "samir@samir.com";
 		String to = "peter@peter.com";
 		String subject = "Invitation Subject";
-		String body = "Body message";
+		String body =
+				"Accept: http://localhost:4567/invitations/1/accept\n"
+			  + "Reject: http://localhost:4567/invitations/1/reject\n";
 		return new Email(from, to, subject, body);
 	}
 }
