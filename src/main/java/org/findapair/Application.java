b64bfddd005cdfc71497b723add44cf37e9dda98
Sandro Mancuso <sandromancuso@yahoo.com>
2014-11-04 00:58:01 +0200

    Application starts PairProgrammingMatchMaking

--- src/main/java/org/findapair/Application.java
+++ src/main/java/org/findapair/Application.java
@@ -1,36 +1,11 @@
 package org.findapair;
 
-import spark.Route;
-
-import static spark.Spark.get;
-import static spark.Spark.post;
-
 public class Application {
 
 	public static void main(String[] args) {
-
-		createRoutes();
-	}
-
-	private static void createRoutes() {
-		Pages pages = new Pages();
-		Emailer emailer = new FakeEmailerToConsole();
-
-		get("/", (req, res) -> pages.findAPair());
-		post("/pairs", (req, res) -> pages.availablePairs());
-		post("/sessions/add", (req, res) -> null);
-
-		formCompatiblePut("/invitations/:id", new InvitePair(pages, emailer));
-		emailCompatiblePost("/invitations/:id/accept", (req, res) -> pages.invitationAccepted());
-		emailCompatiblePost("/invitations/:id/reject", (req, res) -> pages.invitationRejected());
-	}
-
-	private static void formCompatiblePut(String path, Route route) {
-		post(path, route);
-	}
-
-	private static void emailCompatiblePost(String path, Route route) {
-		get(path, route);
+		Routes routes = new Routes();
+		PairProgrammingMatchmaking pairProgrammingMatchmaking = new PairProgrammingMatchmaking(routes);
+		pairProgrammingMatchmaking.start();
 	}
 
 }
--- src/main/java/org/findapair/Routes.java
+++ src/main/java/org/findapair/Routes.java
@@ -1,9 +1,30 @@
 package org.findapair;
 
+import spark.Route;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class Routes {
 
+	private void formCompatiblePut(String path, Route route) {
+		post(path, route);
+	}
+
+	private void emailCompatiblePost(String path, Route route) {
+		get(path, route);
+	}
+
 	public void initialise() {
+		Pages pages = new Pages();
+		Emailer emailer = new FakeEmailerToConsole();
 
+		get("/", (req, res) -> pages.findAPair());
+		post("/pairs", (req, res) -> pages.availablePairs());
+		post("/sessions/add", (req, res) -> null);
 
+		formCompatiblePut("/invitations/:id", new InvitePair(pages, emailer));
+		emailCompatiblePost("/invitations/:id/accept", (req, res) -> pages.invitationAccepted());
+		emailCompatiblePost("/invitations/:id/reject", (req, res) -> pages.invitationRejected());
 	}
 }
