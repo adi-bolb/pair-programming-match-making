369fbb6257e30208beefeff98045d2c52cc2912e
Samir Talwar <samir@noodlesandwich.com>
2014-11-03 20:03:06 +0200

    The in-memory database contains everything you put in it.

--- src/main/java/org/findapair/database/InMemoryDatabase.java
+++ src/main/java/org/findapair/database/InMemoryDatabase.java
@@ -1,18 +1,27 @@
 package org.findapair.database;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 import org.findapair.pairing.AvailablePairingSession;
+import org.findapair.pairing.Id;
 import org.findapair.pairing.Saved;
 
 public final class InMemoryDatabase implements Database {
+    private final Collection<Saved<AvailablePairingSession>> sessions = new HashSet<>();
+    private int nextId = 0;
+
     @Override
     public void add(AvailablePairingSession session) {
-        throw new UnsupportedOperationException();
+        sessions.add(new Saved<>(nextId(), session));
     }
 
     @Override
     public Collection<Saved<AvailablePairingSession>> list() {
-        return Collections.emptyList();
+        return sessions;
+    }
+
+    private Id nextId() {
+        nextId += 1;
+        return new Id(nextId);
     }
 }
--- src/test/java/org/findapair/database/InMemoryDatabaseTest.java
+++ src/test/java/org/findapair/database/InMemoryDatabaseTest.java
@@ -3,23 +3,47 @@ package org.findapair.database;
 import java.util.Collection;
 import org.findapair.pairing.AvailablePairingSession;
 import org.findapair.pairing.Saved;
+import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
 public class InMemoryDatabaseTest {
+    private final InMemoryDatabase database = new InMemoryDatabase();
 
     @Test
-    public void initialDatabaseIsEmpty(){
-        InMemoryDatabase database = new InMemoryDatabase();
-
+    public void initialDatabaseIsEmpty() {
         assertThat(database.list(), is(empty()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void theDatabaseContainsEverythingYouPutInIt() {
+        AvailablePairingSession sessionA = new AvailablePairingSession("Alex", "Hi!");
+        AvailablePairingSession sessionB = new AvailablePairingSession("Bob", "Hello!");
+        AvailablePairingSession sessionC = new AvailablePairingSession("Charlie", "Woop!");
+
+        database.add(sessionA);
+        database.add(sessionB);
+        database.add(sessionC);
+
+        assertThat(database.list(), containsInAnyOrder(saved(sessionA), saved(sessionB), saved(sessionC)));
     }
 
     private static Matcher<Collection<? extends Saved<AvailablePairingSession>>> empty() {
         return Matchers.<Saved<AvailablePairingSession>>empty();
+    }
+
+    private static <T> Matcher<Saved<T>> saved(T value) {
+        return new FeatureMatcher<Saved<T>, T>(sameInstance(value), "value", "value") {
+            @Override protected T featureValueOf(Saved<T> saved) {
+                return saved.value();
+            }
+        };
     }
 }
