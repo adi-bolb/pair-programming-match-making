827d7efdc5e3a1a5438ed9be5df538b70a701b73
Sandro Mancuso <sandromancuso@yahoo.com>
2014-11-04 02:40:58 +0200

    Injected PairingSessionRepository into AddPairingSession

--- src/core/java/org/matchmaking/actions/AddPairingSession.java
+++ src/core/java/org/matchmaking/actions/AddPairingSession.java
@@ -1,11 +1,13 @@
 package org.matchmaking.actions;
 
+import com.google.inject.Inject;
 import org.matchmaking.domain.PairingSession;
 import org.matchmaking.domain.PairingSessionRepository;
 
 public class AddPairingSession {
 	private PairingSessionRepository pairingSessionRepository;
 
+	@Inject
 	public AddPairingSession(PairingSessionRepository pairingSessionRepository) {
 		this.pairingSessionRepository = pairingSessionRepository;
 	}
--- src/core/java/org/matchmaking/domain/PairingSessionRepository.java
+++ src/core/java/org/matchmaking/domain/PairingSessionRepository.java
@@ -1,10 +1,7 @@
 package org.matchmaking.domain;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class PairingSessionRepository {
 
 	public void add(PairingSession pairingSession) {
-		throw new NotImplementedException();
 	}
 }
